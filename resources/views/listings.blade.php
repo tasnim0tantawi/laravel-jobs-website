21c48670840f8d056e3fa9d4c1f8970dc6bc9c64
tasnim <tasnim0tantawi@gmail.com>
2023-07-16 15:46:57 +0300

    migrations

--- resources/views/listings.blade.php
+++ resources/views/listings.blade.php
@@ -54,17 +54,19 @@
             @include('partials._search')
       
             <div class="lg:grid lg:grid-cols-2 gap-4 space-y-4 md:space-y-0 mx-4">
+                @foreach($jobs as $job)
                 <!-- Item 1 -->
+
                 <div class="bg-gray-50 border border-gray-200 rounded p-6">
                     <div class="flex">
                         <img
                             class="hidden w-48 mr-6 md:block"
-                            src="images/acme.png"
+                            src="images/no-image.png"
                             alt=""
                         />
                         <div>
                             <h3 class="text-2xl">
-                                <a href="show.html">Senior Laravel Developer</a>
+                                <a href="show.html">{{$job->title}}</a>
                             </h3>
                             <div class="text-xl font-bold mb-4">Acme Corp</div>
                             <ul class="flex">
@@ -96,8 +98,8 @@
                         </div>
                     </div>
                 </div>
+                @endforeach
  
-
                             
                         </div>
                     </div>
--- routes/web.php
+++ routes/web.php
@@ -13,4 +13,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::view('/', 'welcome');
+Route::view('/', 'listings');
+Route::view('/login', 'login');
+Route::view('/register', 'register');
+
+
